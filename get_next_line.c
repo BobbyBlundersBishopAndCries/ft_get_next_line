6684e3c8ab8359680f2cb0e4dbda8f75d1850eea
mohabid <med080abid@gmail.com>
2024-12-03 00:39:18 +0100

    replaced

--- get_next_line.c
+++ get_next_line.c
@@ -12,32 +12,49 @@
 
 #include "get_next_line.h"
 
-static char	*read_to_buffer(int fd, char **buffer)
-{
-	char	*temp;
-	int		bytes_read;
-	char	*new_buffer;
+static char *read_from_fd(int fd) {
+    char *temp = (char *)malloc(BUFFER_SIZE + 1);
+    if (!temp)
+        return NULL;
 
-	temp = malloc(BUFFER_SIZE + 1);
-	if (!temp)
-		return (NULL);
-	bytes_read = 1;
-	while (!ft_strchr(*buffer, '\n') && bytes_read != 0)
-	{
-		bytes_read = read(fd, temp, BUFFER_SIZE);
-		if (bytes_read == -1)
-		{
-			free(temp);
-			return (NULL);
-		}
-		if (bytes_read == 0)
-			break ;
-		temp[bytes_read] = '\0';
-		new_buffer = ft_strjoin(*buffer, temp);
-		*buffer = new_buffer;
-	}
-	free(temp);
-	return (*buffer);
+    int bytes_read = read(fd, temp, BUFFER_SIZE);
+    if (bytes_read == -1) {
+        free(temp);
+        return NULL;
+    }
+
+    temp[bytes_read] = '\0';
+    return temp;
+}
+
+static char *update_buffer(char **buffer, char *temp) {
+    char *new_buffer = ft_strjoin(*buffer, temp);
+    free(*buffer);
+    *buffer = new_buffer;
+    return *buffer;
+}
+
+static char *read_to_buffer(int fd, char **buffer) {
+    char *temp;
+
+    while (!ft_strchr(*buffer, '\n')) {
+        temp = read_from_fd(fd);
+        if (!temp) {
+            free(*buffer);
+            *buffer = NULL;
+            return NULL;
+        }
+
+        if (*temp == '\0') {
+            free(temp);
+            break;
+        }
+
+        update_buffer(buffer, temp);
+        free(temp);
+    }
+
+    return *buffer;
 }
 
 char	*get_next_line(int fd)
